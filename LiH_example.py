e8e1b99c8940aad08e4799379d8eb502bb631b78
mhirsbrunner <mark@mhirsbrunner.com>
2022-06-26 22:05:51 -0500

    Trying to get the LiH example working with saving/loading good VQE results. For some reason the intermediate_statevector code breaks with the circuits output by the VQEUCCFactory code, giving the following error upon trying to rebuild the circuit:
    qiskit.circuit.exceptions.CircuitError: "Bit 'Qubit(QuantumRegister(4, 'q'), 0)' is not in the circuit."

--- LiH_example.py
+++ LiH_example.py
@@ -13,11 +13,18 @@ from qiskit.algorithms import NumPyMinimumEigensolver
 from qiskit_nature.algorithms import VQEUCCFactory
 from qiskit_nature.algorithms.ground_state_solvers import GroundStateEigensolver
 
-import numpy as np
-
 from qiskit.providers.basicaer import QasmSimulatorPy  # local simulator
 
+import numpy as np
+
+import os
+import pickle as pkl
+
 from src import krylov_vqe as kvqe
+
+# Set 'run_vqe' to true to try and generate a better VQE result
+run_vqe = False
+num_runs = 1
 
 # Specify the molecule
 bond_distance = 2.5  # in Angstrom
@@ -56,19 +63,68 @@ np_result = np_groundstate_solver.solve(problem)
 
 nuclear_repulsion_energy = np_result.nuclear_repulsion_energy
 
-target_energy = np.real(np_result.eigenenergies[0]) + nuclear_repulsion_energy
+exact_ground_eval = np.real(np_result.eigenenergies[0]) + nuclear_repulsion_energy
 print('Done')
-print("Classical Solution Energy:", target_energy)
+print("Classical Solution Energy:", exact_ground_eval)
 
 # VQE
-print('Starting VQE calculation with NumPy solver...')
-vqe_solver = VQEUCCFactory(quantum_instance=QasmSimulatorPy())
-calc = GroundStateEigensolver(qubit_converter, vqe_solver)
+result_fname = 'vqe_circuit_data/LiH_data.pickle'
 
-res = calc.solve(problem)
+if run_vqe:
+    print('Starting VQE runs...')
+    vqe_solver = VQEUCCFactory(quantum_instance=QasmSimulatorPy())
+    calc = GroundStateEigensolver(qubit_converter, vqe_solver)
+
+    # Iterate VQE calculation to find a good optimized circuit
+    for ii in range(num_runs):
+
+        # Try to load old results for comparison
+        if ii == 0:
+            if os.path.isfile(result_fname):
+                with open(result_fname, 'rb') as handle:
+                    vqe_res, optimized_circuit = pkl.load(handle)
+
+                vqe_ground_eval = vqe_res.hartree_fock_energy
+            else:
+                print("No previous VQE results found")
+                vqe_ground_eval = np.abs(exact_ground_eval) + 100
+
+        # Run a VQE calculations
+        res = calc.solve(problem)
+
+        # Compare to old optimized circuit, overwrite old result
+        if np.abs(res.hartree_fock_energy - exact_ground_eval) < np.abs(vqe_ground_eval - exact_ground_eval):
+            vqe_res = res
+            vqe_ground_eval = vqe_res.hartree_fock_energy
+
+            print(f'Finished VQE run {ii + 1}/{num_runs}. Obtained groundstate eval'
+                  f' {res.hartree_fock_energy.real:.4f}. New best!', end='\r')
+
+            optimal_params = vqe_res.raw_result.optimal_point
+            optimized_circuit = calc.solver.ansatz.assign_parameters(optimal_params)
+
+            with open(result_fname, 'wb') as handle:
+                pkl.dump([vqe_res, optimized_circuit], handle)
+
+        else:
+            print(f'Finished VQE run {ii + 1}/{num_runs}. Obtained groundstate eval'
+                  f' {res.hartree_fock_energy.real:.4f} ', end='\r')
+
+    print('\n')
+
+with open(result_fname, 'rb') as handle:
+    res, optimized_circuit = pkl.load(handle)
 
 print(f'VQEUCC Energy: {res.hartree_fock_energy}')
 
-raw_res = res.raw_result
-krylov_vqe_eigenvalue = kvqe.krylov_vqe(qubit_op, raw_res, calc.solver.ansatz)
-print(f'Kylov VQE Eigenvalue:{krylov_vqe_eigenvalue + nuclear_repulsion_energy}')
+krylov_eigenvalue = kvqe.krylov_vqe(qubit_op, optimized_circuit, depth_period=2, decompositions=2)\
+                    + nuclear_repulsion_energy
+
+# Report results
+print(f'Exact Groundstate Eigenvalue: {exact_ground_eval}')
+print(f'Best VQE Groundstate Eigenvalue: {res.hartree_fock_energy}')
+print(f'Krylov Groundstate Eigenvalue:{krylov_eigenvalue}\n')
+
+print(f'Best VQE Eigenvalue error:'
+      f' {100 * np.abs((res.hartree_fock_energy - exact_ground_eval) / exact_ground_eval):.4f}%')
+print(f'Krylov Eigenvalue error: {100 * np.abs((krylov_eigenvalue - exact_ground_eval) / exact_ground_eval):.4f}%')
--- src/krylov_vqe.py
+++ src/krylov_vqe.py
@@ -6,23 +6,29 @@ import numpy as np
 import scipy.linalg as slg
 
 
-def intermediate_vqe_states(optimized_circuit: QuantumCircuit, depth_period=1):
+def intermediate_vqe_states(optimized_circuit: QuantumCircuit, depth_period=1, decompositions=1):
     """
     Extracts the intermediate statevectors from an optimzed VQE circuit by rebuilding the circuit step by step and
     saving the Statevector at desired circuit depth increments
     :param optimized_circuit: The optimized circuit produced by VQE
     :param depth_period: The depth multiples at which the state vector is desired
+    :param decompositions: The number of times to try decomposing the circuit into smaller blocks
     :return: A list of state vectors at each "barrier" instruction of the circuit
     """
+
+    new_circuit = QuantumCircuit(optimized_circuit.num_qubits, optimized_circuit.num_clbits)
+
+    for ii in range(decompositions):
+        optimized_circuit = optimized_circuit.decompose()
 
     if depth_period > optimized_circuit.depth():
         raise ValueError(f'The {depth_period=} cannot be larger than the depth of the optimized VQE'
                          f' circuit={optimized_circuit.depth()}.')
-    new_circuit = QuantumCircuit(optimized_circuit.num_qubits, optimized_circuit.num_clbits)
+
     state_vectors = []
     last_measured_depth = 0
 
-    for instruction in optimized_circuit.decompose().data:
+    for instruction in optimized_circuit.data:
         new_circuit.append(instruction[0], instruction[1], instruction[2])
         current_depth = new_circuit.depth()
         if current_depth - last_measured_depth >= depth_period:
@@ -38,7 +44,7 @@ def intermediate_vqe_states(optimized_circuit: QuantumCircuit, depth_period=1):
     return state_vectors
 
 
-def generalized_eigenvalue_solver(hamiltonian, state_vectors, tol=1e-15):
+def generalized_eigenvalue_solver(hamiltonian, state_vectors, tol=1e-14):
     """
     Solves the generalized eigenvalue problem of the hamiltonian projected into a finite subset of the Hilbert space.
     The generalized eigenvalue problem is of the form H.x = ES.x, where H is the hamiltonian projected into the provided
@@ -76,15 +82,16 @@ def generalized_eigenvalue_solver(hamiltonian, state_vectors, tol=1e-15):
     return evals
 
 
-def krylov_vqe(hamiltonian, optimized_circuit: QuantumCircuit, depth_period=1):
+def krylov_vqe(hamiltonian, optimized_circuit: QuantumCircuit, depth_period=1, decompositions=1):
     """
     Top-level function for solving a generalized eigenvalue problem generated from an optimized VQE circuit
     :param hamiltonian: The Hamiltonian for which the VQE circuit produces the groundstate
     :param optimized_circuit: The optimized circuit produced by VQE
     :param depth_period:
+    :param decompositions: The number of times to try decomposing the circuit into smaller blocks
     :return: The groundstate eigenvalue of the generalized eigenvalue problem
     """
-    state_vectors = intermediate_vqe_states(optimized_circuit, depth_period)
+    state_vectors = intermediate_vqe_states(optimized_circuit, depth_period, decompositions)
     evals = generalized_eigenvalue_solver(hamiltonian, state_vectors)
 
     return min(evals)
